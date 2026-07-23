0b2915bbf63984cfddc2832c5cb85fade29c3e66
Rhuan R. <Rhuanrocha18@gmail.com>
2026-07-23 23:24:19 +0000

    Exercicios

---
+++ Listaexercicios01/Questao03.java
@@ -0,0 +1,24 @@
+/*. Faça um programa em Java que leia 4 notas, mostre as notas e a média na tela. */
+import java.util.Scanner;
+
+public class Questao03{
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double[] notas = new double [4];
+        double soma = 0;
+        for (int i=0; i<notas.length; i++){
+            System.out.print("Digite a nota " +(i+1) + ": ");
+            notas[i] = scanner.nextDouble();
+            soma += notas[i];
+            
+        }
+        double media = soma/notas.length;
+        for (int i=0; i<notas.length;i++){
+            System.out.println("Nota " + (i +1) + ":" + notas[i]);
+           
+        scanner.close();
+
+
+    }System.out.println("A média é: " + media);
+        }
+} 
---
+++ Listaexercicios01/Questao04.java
@@ -0,0 +1,43 @@
+/*4. Faça um programa em Java que leia um vetor de 10 caracteres,
+e diga quantas consoantes foram lidas. Imprima as consoantes.*/
+
+import java.util.Scanner;
+
+public class Questao04 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        char[] letras = new char[10];
+        char[] consoantes = new char[10];
+        int contador = 0;
+
+        // Leitura das letras
+        for (int i = 0; i < letras.length; i++) {
+
+            System.out.print("Digite a letra " + (i + 1) + ": ");
+            letras[i] = scanner.next().charAt(0);
+
+            if (letras[i] != 'a' && letras[i] != 'A' &&
+                letras[i] != 'e' && letras[i] != 'E' &&
+                letras[i] != 'i' && letras[i] != 'I' &&
+                letras[i] != 'o' && letras[i] != 'O' &&
+                letras[i] != 'u' && letras[i] != 'U') {
+
+                consoantes[contador] = letras[i];
+                contador++;
+            }
+        }
+
+        System.out.println("\nConsoantes lidas:");
+
+        for (int i = 0; i < contador; i++) {
+            System.out.println(consoantes[i]);
+        }
+
+        System.out.println("Quantidade de consoantes: " + contador);
+
+        scanner.close();
+    }
+}
